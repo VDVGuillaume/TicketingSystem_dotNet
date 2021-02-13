c70feb00a397bf68dc4f16d6905d8f14d18d6d08
driesbols96 <dries.bols@student.hogent.be>
2021-02-13 18:04:14 +0100

    add test project, remove migrations + unused dbContext

---
+++ TicketingSystem.Xunit.Tests/LoginUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TicketingSystem.Xunit.Tests
+{
+    public class LoginUnitTests
+    {
+        [Fact]
+        public void Should_Login() 
+        {
+            var mock = new Mock<IMediator>();
+
+            //mock.Setup();
+
+        }
+
+    }
+}
