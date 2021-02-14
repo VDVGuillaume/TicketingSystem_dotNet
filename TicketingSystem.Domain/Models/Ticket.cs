5c267412d2100aa457203e7295ad641e17ce1d6c
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-02-14 15:08:10 +0100

    Ticket class created + extra classes for attachment & client.

---
+++ TicketingSystem.Domain/Models/Ticket.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketingSystem.Domain.Models
+{
+
+    public enum Status
+    {
+        Aangemaakt,
+        InBehandeling,
+        Afgehandeld,
+        Geannuleerd
+    }
+
+
+    public class Ticket
+    {
+        public int Ticketnr;
+        public string Title;
+        public Status Status;
+        public DateTime DateAdded;
+        public string Description;
+        public Client Client;
+        public string AssignedEngineer;
+        public string Type;
+        public List<string> Comments;
+        public List<Attachment> Attachments;
+
+
+        public Ticket(string title, string description, string  type, Attachment attachment = null)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.Type = type;
+            this.DateAdded = DateTime.Now;
+            Attachments.Add(attachment);
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
