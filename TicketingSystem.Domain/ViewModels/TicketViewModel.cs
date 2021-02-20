3a910272f2a760721cd3210002c9762f05f13571
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-02-20 10:38:01 +0100

    TicketController start implementation + created TicketViewModel

---
+++ TicketingSystem.Domain/ViewModels/TicketViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketingSystem.Domain.ViewModels
+{
+    public class TicketViewModel
+    {
+    }
+}
--- TicketingSystem.RazorWebsite/Controllers/TicketController.cs
+++ TicketingSystem.RazorWebsite/Controllers/TicketController.cs
@@ -1,16 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TicketingSystem.Domain.ViewModels;
 
 namespace TicketingSystem.RazorWebsite.Controllers
 {
     public class TicketController : Controller
     {
+
+        private readonly ILogger<TicketViewModel> _logger;
+        private readonly IMediator _mediator;
+
+        public TicketController(
+            ILogger<TicketViewModel> logger,            
+            IMediator mediator)
+        {           
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+
+
         public IActionResult Index()
         {
             return View();
         }
+
+
+
+        [HttpPost]
+        public IActionResult CreateTicket(TicketViewModel model)
+        {
+            
+            
+            return View(model);
+        }
+
     }
 }
