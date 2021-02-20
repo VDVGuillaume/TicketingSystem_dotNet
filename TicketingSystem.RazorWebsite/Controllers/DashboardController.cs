99ae67f8926732cbe4770324ead226d036fb833c
driesbols96 <dries.bols@student.hogent.be>
2021-02-20 01:18:52 +0100

    Add dashboard controller, edit dashboard link

---
+++ TicketingSystem.RazorWebsite/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TicketingSystem.RazorWebsite.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
