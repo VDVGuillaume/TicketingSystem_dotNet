719ca0817b6c16d71b6422e056b1f2517122651a
driesbols96 <dries.bols@student.hogent.be>
2021-02-21 01:51:15 +0100

    add get current user to TicketController

--- TicketingSystem.RazorWebsite/Controllers/TicketController.cs
+++ TicketingSystem.RazorWebsite/Controllers/TicketController.cs
@@ -1,7 +1,9 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 using TicketingSystem.RazorWebsite.Models;
 
 namespace TicketingSystem.RazorWebsite.Controllers
@@ -9,16 +11,18 @@ namespace TicketingSystem.RazorWebsite.Controllers
     [Authorize(Roles = "Customer,SupportManager")]
     public class TicketController : Controller
     {
-
         private readonly ILogger<TicketViewModel> _logger;
         private readonly IMediator _mediator;
+        private readonly UserManager<IdentityUser> _userManager;
 
         public TicketController(
             ILogger<TicketViewModel> logger,            
-            IMediator mediator)
+            IMediator mediator,
+            UserManager<IdentityUser> userManager)
         {           
             _logger = logger;
             _mediator = mediator;
+            _userManager = userManager;
         }
 
         [Authorize(Roles = "Customer,SupportManager")]
@@ -36,8 +40,11 @@ namespace TicketingSystem.RazorWebsite.Controllers
 
         [Authorize(Roles = "Customer,SupportManager")]
         [HttpPost]
-        public IActionResult CreateTicket(TicketViewModel model)
+        public async Task<IActionResult> CreateTicket(TicketViewModel model)
         {
+            // get current user
+            var user = await _userManager.GetUserAsync(User);
+
             return View(model);
         }
 
