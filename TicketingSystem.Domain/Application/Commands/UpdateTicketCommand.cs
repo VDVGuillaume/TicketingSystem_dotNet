5950232b0da7b696b6148af499e0ad752bb00882
Thomas De Neve <thomas.deneve@student.hogent.be>
2021-02-23 23:13:47 +0100

    UpdateTicketCommand

---
+++ TicketingSystem.Domain/Application/Commands/UpdateTicketCommand.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using TicketingSystem.Domain.Models;
+
+namespace TicketingSystem.Domain.Application.Commands
+{
+    public class UpdateTicketCommand : BaseCommand<Ticket>
+    {
+        public int Ticketnr;
+        public string Title;
+        public string Description;
+        public string AssignedEngineer;
+        public string Type;
+    }
+}
---
+++ TicketingSystem.Infrastructure/CommandHandlers/UpdateTicketCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TicketingSystem.Domain.Application.Commands;
+using TicketingSystem.Domain.Models;
+
+namespace TicketingSystem.Infrastructure.CommandHandlers
+{
+    public class UpdateTicketCommandHandler : BaseCommandHandler<UpdateTicketCommand, Ticket>
+    {
+        public UpdateTicketCommandHandler(IMediator mediator, TicketingSystemDbContext dbContext) : base(mediator, dbContext)
+        {
+        }
+
+        public async override Task<Ticket> ExecuteCommandAsync(UpdateTicketCommand request, CancellationToken cancellationToken)
+        {
+            var ticket = _dbContext.Tickets.First(x => x.Ticketnr == request.Ticketnr);
+            ticket.Description = request.Description;
+            _dbContext.Tickets.Update(ticket);
+            await _dbContext.SaveChangesAsync();
+
+            return ticket;
+        }
+    }
+}
