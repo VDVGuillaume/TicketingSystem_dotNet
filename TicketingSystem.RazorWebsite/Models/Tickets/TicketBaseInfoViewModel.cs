a68d4c0ada8dd0dec8204b0a96ce40c2fdd2f43a
driesbols96 <dries.bols@student.hogent.be>
2021-02-24 21:20:00 +0100

    add different colors to ticket statuses

--- TicketingSystem.RazorWebsite/Models/Tickets/TicketBaseInfoViewModel.cs
+++ TicketingSystem.RazorWebsite/Models/Tickets/TicketBaseInfoViewModel.cs
@@ -5,5 +5,20 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Status { get; set; }
+
+        public string StatusColor {get 
+            { switch (Status) 
+                {
+                    case "Aangemaakt":
+                        return "green"; 
+                    case "InBehandeling":
+                        return "sandybrown";
+                    case "Geannuleerd":
+                        return "tomato";
+                    default:
+                        return "darkgrey";
+                }
+            }
+        }
     }
 }
