e5da09c37ef33eb01e2a19f7994b064dec0ae9f9
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-02-24 18:10:17 +0100

    Added start TicketControllerTest

---
+++ TicketingSystem.Xunit.Tests/Controllers/TicketControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketingSystem.RazorWebsite.Controllers;
+using TicketingSystem.Xunit.Tests.Data;
+
+namespace TicketingSystem.Xunit.Tests.Controllers
+{
+    public class TicketControllerTest
+    {
+        private readonly TicketController _controller;
+        private readonly Mock<IMediator> _mediatr;        
+        private readonly DummyApplicationDbContext _dummyContext;
+
+
+
+    }
+}
