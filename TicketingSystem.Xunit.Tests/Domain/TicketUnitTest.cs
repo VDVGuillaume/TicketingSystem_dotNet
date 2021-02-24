2d6fbfd37164cd040d47c4674b9099dc9f58af7b
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-02-24 18:10:36 +0100

    Added start TicketUnitTest

---
+++ TicketingSystem.Xunit.Tests/Domain/TicketUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketingSystem.Domain.Models;
+using Xunit;
+
+namespace TicketingSystem.Xunit.Tests.Domain
+{
+    public class TicketUnitTest
+    {
+
+
+        [Fact]
+        public void CreateTicket_ValidSupportContract_TicketCreated()
+        {
+            
+        }
+
+
+        [Fact]
+        public void CreateTicket_InvalidSupportContract_ThrowsException()
+        {
+
+            TicketType bug = new TicketType();
+           // Assert.Throws<InvalidOperationException>(() => new Ticket("test","Dit is een testTicket",bug,));
+        }
+
+
+
+
+
+    }
+}
