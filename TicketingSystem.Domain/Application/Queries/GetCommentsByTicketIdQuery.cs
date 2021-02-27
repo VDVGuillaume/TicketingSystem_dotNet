11c8712de7c08bc7e1f71d4a998c66f956c3958b
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-02-27 17:38:42 +0100

    Added getCommentsByTicketIdQueryHandler

---
+++ TicketingSystem.Domain/Application/Queries/GetCommentsByTicketIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+using TicketingSystem.Domain.Models;
+
+namespace TicketingSystem.Domain.Application.Queries
+{
+    public class GetCommentsByTicketIdQuery : BaseQuery<IQueryable<Comment>>
+    {
+        public int Id { get; set; }
+    }
+}
