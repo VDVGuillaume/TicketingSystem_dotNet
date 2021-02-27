c6bcbf7afbecacebbe8b4ad72d79a68fc1c6b72c
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-02-27 15:37:43 +0100

    Added Create Comment Command

---
+++ TicketingSystem.Domain/Application/Commands/CreateCommentCommand.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TicketingSystem.Domain.Models;
+
+namespace TicketingSystem.Domain.Application.Commands
+{
+    class CreateCommentCommand : BaseCommand<Comment>
+    {
+        public string Text;
+        public DateTime DateAdded;
+        public IdentityUser Client;
+    }
+}
