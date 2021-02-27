6598736cf0f973ea4e2db53c1f81d43186ed81f9
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-02-27 15:37:28 +0100

    Added extra properties to comment class (DateAdded, CreatedBy)

--- TicketingSystem.Domain/Models/Comment.cs
+++ TicketingSystem.Domain/Models/Comment.cs
@@ -1,9 +1,14 @@
-﻿namespace TicketingSystem.Domain.Models
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+
+namespace TicketingSystem.Domain.Models
 {
     public class Comment
     {
-        public int CommentId;
-        public string text;
-
+        public int CommentId { get; set; }
+        public string Text { get; set; }
+        public DateTime DateAdded { get; set; }
+        public IdentityUser CreatedBy { get; set; }
     }
 }
