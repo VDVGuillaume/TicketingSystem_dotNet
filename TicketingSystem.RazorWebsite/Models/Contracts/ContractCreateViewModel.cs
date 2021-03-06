94bb293e545b25ea1abfc893336be2d5dfc5e2b3
driesbols96 <dries.bols@student.hogent.be>
2021-03-06 10:47:40 +0100

    Add create contract view model + view

---
+++ TicketingSystem.RazorWebsite/Models/Contracts/ContractCreateViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketingSystem.RazorWebsite.Models.Contracts
+{
+    public class ContractCreateViewModel
+    {
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        public List<SelectListItem> ContractTypes { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [Display(Name = "Startdatum")]
+            public DateTime StartDate { get; set; }
+            [Required]
+            [Display(Name = "Einddatum")]
+            public DateTime EndDate { get; set; }
+            [Required]
+            [Display(Name = "Type")]
+            public string Type { get; set; }
+            [Display(Name = "Klant")]
+            public string ClientName { get; set; }
+        }
+    }
+}
