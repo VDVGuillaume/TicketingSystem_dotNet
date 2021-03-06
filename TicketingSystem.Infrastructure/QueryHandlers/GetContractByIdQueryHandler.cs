adf173d1a7f3259271d6012bd100265c6ea89e2a
Thomas De Neve <thomas.deneve@student.hogent.be>
2021-03-06 15:02:04 +0100

    Create GetContractByIdQueryHandler.cs

---
+++ TicketingSystem.Infrastructure/QueryHandlers/GetContractByIdQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TicketingSystem.Domain.Application.Queries;
+using TicketingSystem.Domain.Models;
+
+namespace TicketingSystem.Infrastructure.QueryHandlers
+{
+    public class GetContractByIdQueryHandler : BaseQueryHandler<GetContractByIdQuery, Contract>
+    {
+        public GetContractByIdQueryHandler(IMediator mediator, TicketingSystemDbContext dbContext) : base(mediator, dbContext)
+        {
+        }
+
+        public async override Task<Contract> ExecuteCommandAsync(GetContractByIdQuery request, CancellationToken cancellationToken)
+        {
+            return _dbContext.Contracts
+                .Include(x => x.Type)
+                .Include(x => x.Client)
+                .FirstOrDefault(x => x.ContractId == request.Id);
+        }
+    }
+}
