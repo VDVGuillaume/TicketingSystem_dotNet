ca40253ea97c59e922b1c82830c0fad502cd1319
driesbols96 <dries.bols@student.hogent.be>
2021-03-06 17:44:52 +0100

    remove unused params in createticketcommand

--- TicketingSystem.Domain/Application/Commands/CreateTicketCommand.cs
+++ TicketingSystem.Domain/Application/Commands/CreateTicketCommand.cs
@@ -9,11 +9,8 @@ namespace TicketingSystem.Domain.Application.Commands
     public class CreateTicketCommand : BaseCommand<Ticket>
     {
         public string Title { get; set; }
-        public TicketStatus Status { get; set; }
-        public DateTime DateAdded { get; set; }
         public string Description { get; set; }
         public Client Client { get; set; }
-        public string Username { get; set; }
         public string AssignedEngineer { get; set; }
         public string Type { get; set; }
         public List<IFormFile> Attachments { get; set; }
