47fb8068f776552479d7f471d24f3d38e6edb100
Thomas De Neve <thomas.deneve@student.hogent.be>
2021-03-06 14:26:47 +0100

    Add GetContractsQuery

---
+++ TicketingSystem.Domain/Application/Queries/GetContractsQuery.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+using TicketingSystem.Domain.Models;
+
+namespace TicketingSystem.Domain.Application.Queries
+{
+    public class GetContractsQuery : BaseQuery<IQueryable<Contract>>
+    {
+    }
+}
---
+++ TicketingSystem.Infrastructure/QueryHandlers/GetContractsQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TicketingSystem.Domain.Application.Queries;
+using TicketingSystem.Domain.Models;
+
+namespace TicketingSystem.Infrastructure.QueryHandlers
+{
+    public class GetContractsQueryHandler : BaseQueryHandler<GetContractsQuery, IQueryable<Contract>>
+    {
+        public GetContractsQueryHandler(IMediator mediator, TicketingSystemDbContext dbContext) : base(mediator, dbContext)
+        {
+        }
+
+        public async override Task<IQueryable<Contract>> ExecuteCommandAsync(GetContractsQuery request, CancellationToken cancellationToken)
+        {
+            return _dbContext.Contracts
+                .Include(x => x.Type)
+                .Include(x => x.Client);
+        }
+    }
+}
