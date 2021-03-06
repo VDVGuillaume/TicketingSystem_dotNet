06ed2c3efb64f60aedd724712eba2712a96d8ddf
Thomas De Neve <thomas.deneve@student.hogent.be>
2021-03-06 20:41:05 +0100

    GetTechnicianUsers Query

---
+++ TicketingSystem.Domain/Application/Queries/GetTechnicianUsersQuery.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+using TicketingSystem.Domain.Models;
+
+namespace TicketingSystem.Domain.Application.Queries
+{
+    public class GetTechnicianUsersQuery : BaseQuery<IQueryable<ApplicationUser>>
+    {
+    }
+}
