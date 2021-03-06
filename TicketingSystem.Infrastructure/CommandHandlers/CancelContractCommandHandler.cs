00b4a4d09f66d11e35def601ceeb8924297b8be1
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-03-06 14:24:09 +0100

    Added Cancel & Create command handlers

---
+++ TicketingSystem.Infrastructure/CommandHandlers/CancelContractCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TicketingSystem.Domain.Application.Commands;
+using TicketingSystem.Domain.Models;
+using TicketingSystem.Infrastructure.Services;
+using TicketingSystem.Infrastructure.Services.Interfaces;
+
+namespace TicketingSystem.Infrastructure.CommandHandlers
+{
+    public class CancelContractCommandHandler : BaseCommandHandler<CancelContractCommand, Contract>
+    {
+        private IContractService _contractService;
+        public CancelContractCommandHandler(IMediator mediator, TicketingSystemDbContext dbContext, IContractService contractService) : base(mediator, dbContext)
+        {
+            _contractService = contractService ?? throw new ArgumentNullException();
+        }
+
+        public async override Task<Contract> ExecuteCommandAsync(CancelContractCommand request, CancellationToken cancellationToken)
+        {
+            return await _contractService.CancelContract(request);
+        }
+    }
+}
+
+
---
+++ TicketingSystem.Infrastructure/CommandHandlers/CreateContractCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TicketingSystem.Domain.Application.Commands;
+using TicketingSystem.Domain.Models;
+using TicketingSystem.Infrastructure.Services;
+using TicketingSystem.Infrastructure.Services.Interfaces;
+
+namespace TicketingSystem.Infrastructure.CommandHandlers
+{
+    public class CreateContractCommandHandler : BaseCommandHandler<CreateContractCommand, Contract>
+    {
+        private IContractService _contractService;
+        public CreateContractCommandHandler(IMediator mediator, TicketingSystemDbContext dbContext, IContractService contractService) : base(mediator, dbContext)
+        {
+            _contractService = contractService ?? throw new ArgumentNullException();
+        }
+
+        public async override Task<Contract> ExecuteCommandAsync(CreateContractCommand request, CancellationToken cancellationToken)
+        {
+            return await _contractService.CreateContract(request);
+        }
+    }
+}
