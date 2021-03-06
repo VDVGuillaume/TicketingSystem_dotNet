4cdb4a9e58f85239faf9f28ae40e2363fe558950
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-03-06 14:23:52 +0100

    Added Contract service interface & concrete class

---
+++ TicketingSystem.Infrastructure/Services/ContractService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TicketingSystem.Domain.Application.Commands;
+using TicketingSystem.Domain.Models;
+using TicketingSystem.Infrastructure.Services.Interfaces;
+
+namespace TicketingSystem.Infrastructure.Services
+{
+    public class ContractService : IContractService
+    {
+        public Task<Contract> CancelContract(CancelContractCommand request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Contract> CreateContract(CreateContractCommand request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TicketingSystem.Infrastructure/Services/Interfaces/IContractService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using TicketingSystem.Domain.Application.Commands;
+using TicketingSystem.Domain.Models;
+
+namespace TicketingSystem.Infrastructure.Services.Interfaces
+{
+    public interface IContractService
+    {
+        Task<Contract> CreateContract(CreateContractCommand request);    
+        Task<Contract> CancelContract(CancelContractCommand request);
+      
+    }
+}
