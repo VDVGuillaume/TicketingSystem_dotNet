b87c7f6981360182015e1b3a037d88d3a28c4806
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-03-06 14:02:27 +0100

    Added Query to get active contracts for a client

---
+++ TicketingSystem.Domain/Application/Queries/GetActiveContractByClientIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TicketingSystem.Domain.Models;
+
+namespace TicketingSystem.Domain.Application.Queries
+{
+    public class GetActiveContractByClientIdQuery :  BaseQuery<IQueryable<Contract>>
+    {
+        public int ClientId { get; set; }
+        
+    }
+}
