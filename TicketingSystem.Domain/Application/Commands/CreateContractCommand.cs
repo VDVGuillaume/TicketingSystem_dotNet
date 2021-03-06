6ca99fd025cc4b8814dff18e05012c8582975c65
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-03-06 10:44:05 +0100

    Added CreateContractCommand

---
+++ TicketingSystem.Domain/Application/Commands/CreateContractCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TicketingSystem.Domain.Models;
+
+namespace TicketingSystem.Domain.Application.Commands
+{
+    public class CreateContractCommand : BaseCommand<Contract>
+    {
+        public ContractType Type { get; set; }
+        public ContractStatus Status { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+        public Client Client { get; set; }
+
+    }
+}
