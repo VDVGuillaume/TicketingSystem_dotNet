3f62794d6cc91ccdbd25b267af59ac59971102d0
Thomas De Neve <thomas.deneve@student.hogent.be>
2021-03-06 19:49:48 +0100

    GetClients Query

---
+++ TicketingSystem.Infrastructure/QueryHandlers/GetClientsQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TicketingSystem.Domain.Application.Queries;
+using TicketingSystem.Domain.Models;
+
+namespace TicketingSystem.Infrastructure.QueryHandlers
+{
+    public class GetClientsQueryHandler : BaseQueryHandler<GetClientsQuery, IQueryable<Client>>
+    {
+        public GetClientsQueryHandler(IMediator mediator, TicketingSystemDbContext dbContext) : base(mediator, dbContext)
+        {
+        }
+
+        public async override Task<IQueryable<Client>> ExecuteCommandAsync(GetClientsQuery request, CancellationToken cancellationToken)
+        {
+            return _dbContext.Client;
+        }
+    }
+}
