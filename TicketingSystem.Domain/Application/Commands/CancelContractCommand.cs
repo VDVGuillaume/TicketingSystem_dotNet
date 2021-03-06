3f2f0b82e8f37e8d5f44771bc45d3f7a286a5c7b
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-03-06 14:12:00 +0100

    Added CancelContractCommand

---
+++ TicketingSystem.Domain/Application/Commands/CancelContractCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TicketingSystem.Domain.Models;
+
+namespace TicketingSystem.Domain.Application.Commands
+{
+    public class CancelContractCommand : BaseCommand<Contract>
+    {
+        public int ContractId { get; set; }
+    }
+}
