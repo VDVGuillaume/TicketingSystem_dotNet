784d5a0956a402a2e8bb51db74f47e0dac9bb486
Guillaume Van de Velde <guillaume.vandevelde@student.hogent.be>
2021-03-06 10:46:40 +0100

    Added GetContractByClientIdQuery

---
+++ TicketingSystem.Domain/Application/Queries/GetContractsByClientIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TicketingSystem.Domain.Models;
+
+namespace TicketingSystem.Domain.Application.Queries
+{
+    public class GetContractsByClientIdQuery : BaseQuery<IQueryable<Contract>>
+    {
+        public int ClientId { get; set; }
+    }
+}
